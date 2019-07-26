a9c0ed781622b44ad3a7c3db79eaa432c29eab0e
yuceltoluyag <ytoluyag@gmail.com>
2019-07-27 01:54:09 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>Yt~Blogger Botu M1</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+<body>
+  
+
+
+<?php
+/**
+  echo '<pre>';
+  print_r();
+  echo '</pre>';
+ * 
+ */
+
+ini_set("max_execution_time", 0);
+
+
+  include "fonksiyon.php";
+//set_time_limit(60);
+
+  for ($k=1; $k<=1 ; $k++) { 
+
+  	if ($k==1) {
+  		$url ="http://gamersonlinux.com/forum/forums/guides.20/";
+  	}else {
+
+  		$url = "http://gamersonlinux.com/forum/forums/guides.20/page-$k";
+
+  	}
+
+  	$tmm = "http://gamersonlinux.com/forum/";
+  	$site = "http://gamersonlinux.com/forum/forums/guides.20/";
+
+$bag = Baglan($url);  //hedef sitemiz
+preg_match_all('@<ol class="discussionListItems">(.*?)</ol>@si',$bag,$konular);  //tüm konu seçkesi
+echo "<h1>$k.Sayfa</h1>";
+
+for ($i=0; $i <count($konular[1]) ; $i++) { 
+preg_match_all('@<h3 class="title">(.*?)</h3>@si',$konular[1][$i],$basliklar);  //tüm başlıklar
+//print_r($basliklar);
+preg_match_all('@<h3 class="title"><a href="(.*?)"(.*?)</h3>@si',$konular[1][$i],$linkler);
+
+
+        //başlıkları temizle
+		$baslik   = duzenle($basliklar[0]);
+		$temizbas = preg_replace('/<a href=\"(.*?)\">(.*?)<\/a>/', "\\2", $baslik); //temiz başlık print_r ($temizbas);
+         for ($z=0; $z<=7; $z++) {          	# code...
+         
+        $linkbas  = $tmm.'/'.$linkler[1][$z];
+        //echo '<pre>';
+       // print_r($linkbas);
+        //echo '</pre>';
+
+        
+
+
+        $bag = Baglan($linkbas);
+      
+       preg_match_all('@<div class="messageContent">(.*?)</div>@si',$bag,$icerik);
+       preg_match_all('@<div class="titleBar"><h1>(.*?)</h1>@si',$bag,$baslik);
+        //print_r($baslik[0][0]).'<br><br>';
+       // print_r($icerik[0][0]);
+
+$salla = $baslik[0][0];
+$sic   =  $icerik[0][0];
+
+?>
+
+
+<form action="https://www.blogger.com/blog-this.do?" method="post">
+    <input type="text" class="sinput" name="n" value="<?php  echo strip_tags($salla); ?>" >   
+    <textarea name="b" class="defter" id="" cols="30" rows="10"><?php echo $sic;?></textarea>  
+    <input type="submit" value="Bloggere Gönder" />
+ </form>
+
+
+ <?php
+
+
+}
+	}
+}
+
+
+
+
+?>
+</body>
+</html>
---
+++ yedek.php
@@ -0,0 +1,60 @@
+
+
+<?php 
+set_time_limit(0);
+
+/**
+ *  echo '<pre>';
+   print_r($gug);
+   echo '</pre>'; 
+ */
+
+
+	$url="http://sourcedigit.com/how-to/page/6/";
+	$veri = file_get_contents($url);
+
+
+	
+	//$botetiket = preg_match_all('@<span class="entry-tags">(.*?)</span>@si',$veri,$etiket);
+	$linki   = preg_match_all('@<div class="entry-content" itemprop="text"><a class="entry-image-link" href="(.*?)" aria-hidden="true"><img width="150" height="150" src="(.*?)" class="alignleft post-image entry-image" alt="(.*?)" itemprop="image" /></a>@si',$veri,$linkibul);
+
+	
+	//$bbet = strip_tags($etiket[0]);
+	$gug  = $linkibul[1];
+
+	//echo "<h1>$gug</h1><hr>";
+	//echo '<textarea name="icerik" id="" cols="30" rows="10">'.$icerik.'</textarea>';
+   		$say=count($gug);
+   		
+   if ($say>0) {
+   			
+   			for ($i=0; $i<$say; $i++) { 
+   				//Linkleri Çekiyoruz
+   				echo "<br>";
+   				$git = $gug[$i].'</br>';
+   				$bascik = file_get_contents($git);
+   				$botbaslik = preg_match_all('@<h2 class="entry-title" itemprop="headline"><a href="(.*?)">(.*?)</a></h2>@si',$veri,$baslik);
+          $boticerik = preg_match_all('@<div class="entry-content" itemprop="text"><p>(.*?)</p></div>@si',$gug[0],$icerik);
+                 
+                 print_r($icerik);
+
+
+                 $baslink = $baslik[0];
+
+                 // başlık echo strip_tags($baslink).'<br>';
+   				
+                      
+   				
+
+   				
+   			}
+   		}else {
+
+   			echo 'içerik Bulunamadı Hacı..';
+   		}
+   		
+
+
+
+
+?>
